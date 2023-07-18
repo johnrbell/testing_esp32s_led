913174c70ed3efcb48b45b8277de16048f85abaf
johnrbell <johnrbell@gmail.com>
2023-07-18 02:13:29 -0400

    onboard GPIO led test

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#define LED_PIN 2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200); delay(1000);
+  Serial.println("welcome to flavortown. 🧇");
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
